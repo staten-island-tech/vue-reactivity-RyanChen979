00b6bec2e7e19157a50caa2457d3f23ec51c1189
Ryan Chen <ryanchen979@gmail.com>
2023-03-10 10:16:22 -0500

    sca

--- vue-reactivity/src/App.vue
+++ vue-reactivity/src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
--- vue-reactivity/src/views/AboutView.vue
+++ vue-reactivity/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Not sure what to do</h1>
   </div>
 </template>
 
