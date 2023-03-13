41076c8add660f05e4d0f88f0cb3fb69ed8791f9
Ryan Chen <ryanchen979@gmail.com>
2023-03-13 10:15:30 -0400

    a

---
+++ vue-reactivity/src/views/SampleTest.vue
@@ -0,0 +1,16 @@
+<template>
+  <SampleTest :list="list" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        list: ["sodfd", "sdb"];
+    }
+
+  },
+};
+</script>
+
+<style scoped></style>
