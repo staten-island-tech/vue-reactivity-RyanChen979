23eb63d253f3b422755f425b23ef50199e81d6e2
Ryan Chen <ryanchen979@gmail.com>
2023-03-14 10:20:35 -0400

    bruh

--- vue-reactivity/src/components/TheWelcome.vue
+++ vue-reactivity/src/components/TheWelcome.vue
@@ -1,15 +1,8 @@
-<script setup>
-import WelcomeItem from "./WelcomeItem.vue";
-import DocumentationIcon from "./icons/IconDocumentation.vue";
-import ToolingIcon from "./icons/IconTooling.vue";
-import EcosystemIcon from "./icons/IconEcosystem.vue";
-import CommunityIcon from "./icons/IconCommunity.vue";
-import SupportIcon from "./icons/IconSupport.vue";
-</script>
+<script setup></script>
 
 <template>
   <WelcomeItem>
-    <template #icon>
+    <template>
       <DocumentationIcon />
     </template>
     <template #heading>Documentation</template>
