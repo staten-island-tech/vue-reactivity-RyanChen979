556fc84dc3d0f9e4d34bde4bf4deec5ff1b5078a
Ryan Chen <ryanchen979@gmail.com>
2023-03-17 10:16:14 -0400

    a

---
+++ vue-reactivity/src/components/ItemsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <ol>
+    <li v-for="items in items">
+      Name: {{ items.name }}; Value: {{ items.value }}
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { name: "1", value: "hgj", id: 1, img: "" },
+        { name: "2", value: "ehgfwr", id: 2, img: "" },
+        { name: "3", value: "tehgfw", id: 2, img: "" },
+        { name: "5", value: "dsdsff", id: 2, img: "" },
+        { name: "4", value: "qdsfw", id: 2, img: "" },
+        { name: "6", value: "ddsfsf", id: 2, img: "" },
+        { name: "7", value: "dsf", id: 2, img: "" },
+        { name: "8", value: "ewr", id: 2, img: "" },
+        { name: "9", value: "tysf", id: 2, img: "" },
+        { name: "10", value: "svcbdf", id: 2, img: "" },
+        { name: "11", value: "edfwr", id: 2, img: "" },
+      ],
+    };
+  },
+};
+</script>
--- vue-reactivity/src/views/HomeView.vue
+++ vue-reactivity/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+import ItemsList from "../components/ItemsList.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ItemsList />
   </main>
 </template>
