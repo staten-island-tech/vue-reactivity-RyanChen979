3804069ba9911d4beb7563d76ad806f21812b671
Ryan Chen <ryanchen979@gmail.com>
2023-03-24 10:16:32 -0400

    a

--- vue-reactivity/src/components/ItemsList.vue
+++ vue-reactivity/src/components/ItemsList.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="everything">
-    <div class="card" v-for="list in computers">
-      <div class="card-inner">
-        <div class="card-front">
-          <h2>Name: {{ list.name }}</h2>
-          <h3>Price: ${{ list.price }}</h3>
-          <img :src="list.image" alt="" /><br />
-          <button type="submit">Details</button>
-        </div>
-        <div class="card-back">
-          <img :src="list.image" alt="" />
-        </div>
+    <div class="card" v-for="list in computers" data-aos="zoom-out-up">
+      <h2>Name: {{ list.name }}</h2>
+      <h3>Price: ${{ list.price }}</h3>
+      <img :src="list.image" :alt="list.name" /><br />
+      <!-- <button type="submit">Details</button> -->
+      <div>
+        <h4 class="details">Details:</h4>
+        <p>Processor: {{ list.details.processor }}</p>
+        <p>Ram: {{ list.details.memory }}</p>
+        <p>Hard Drive: {{ list.details.harddrive }}</p>
+        <p>Color: {{ list.details.color }}</p>
       </div>
     </div>
   </div>
@@ -23,133 +23,230 @@ export default {
       computers: [
         {
           name: "Dell XPS 13 Plus 9320",
-          price: 1149.0,
-          category: "laptops",
+          price: 1499.0,
           image:
             "https://www.notebookcheck.net/uploads/tx_nbc2/xps_13_plus_9320.jpg",
           details: {
-            processor: "",
-            software: "",
-            memory: "",
+            processor:
+              "12th Gen Intel Core i7 1260P (16 threads, 4.70 GHz Turbo)",
+            memory: "16GB LPDDR5, 5200 MHz, integrated, dual channel",
+            harddrive: "1TB, M.2, Gen 4 PCle NVMe, SSD",
+            color: "platinum",
           },
         },
         {
           name: "Lenovo Slim 7",
           price: 873.99,
-          category: "laptops",
           image:
             "https://p4-ofp.static.pub/ShareResource/na/subseries/gallery/lenovo-slim-7i-14inch-01.png",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "Dell G15",
           price: 1099.99,
-          category: "laptops",
           image:
             "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/g-series/g15-5525/pdp/laptop-g-series-15-5525-pdp-hero-bk-coralkb.psd?qlt=95&fit=constrain,1&hei=349&wid=504&fmt=jpg&wid=570&hei=400",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "HP Victus 15 (2022)",
-          price: 0,
-          category: "laptops",
+          price: 549.99,
           image:
             "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZRgmj+NqL.jpg",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "MacBook Air",
-          price: 0,
-          category: "laptops",
+          price: 1199.0,
           image:
-            "https://i.pcmag.com/imagery/reviews/02pPlSC851BxRP4BFdufSBJ-3.fit_lim.size_1050x.jpg",
-        },
-        {
-          name: " ",
-          price: 0,
-          category: "laptops",
-          image: "",
-        },
-        {
-          name: "",
-          price: 0,
-          category: "laptops",
-          image: "",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo1ZAKTLP5A_guGxt05oUjilK8VzSk4z-UQQ&usqp=CAU",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
+        },
+        {
+          name: "",
+          price: 0,
+          image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
+        },
+        {
+          name: "",
+          price: 0,
+          image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "",
           price: 0,
           category: "electronics",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: " ",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: " ",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
         {
           name: "",
           price: 0,
-          category: "laptops",
           image: "",
+          details: {
+            processor: "",
+            memory: "",
+            harddrive: "",
+            color: "",
+          },
         },
       ],
     };
@@ -166,55 +263,29 @@ export default {
 }
 
 .card {
-  background-color: transparent;
-  width: 20%;
-  height: 300px;
-  perspective: 1000px;
-  margin: 1.45rem;
-}
-
-.card-inner {
   display: flex;
   flex-wrap: wrap;
   flex-direction: column;
-
-  margin: 1rem;
+  justify-content: flex-start;
+  width: 20%;
+  height: auto;
+  margin: 1.45rem;
   border: 2px black solid;
-
-  position: relative;
-  width: 100%;
-  height: 100%;
   text-align: center;
-  transition: transform 0.6s;
-  transform-style: preserve-3d;
-}
-.card:hover .card-inner {
-  transform: rotateY(180deg);
+  align-items: center;
 }
 
-.card-front,
-.card-back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
-  border-radius: 16px;
-}
-
-.card-back {
-  transform: rotateY(180deg);
-}
-
-.card-front img {
+img {
   object-fit: cover;
   border-radius: inherit;
   width: 15rem;
 }
-.card-front button {
+
+.details {
+  text-decoration: underline;
 }
 
-.card-back img {
-  width: 15rem;
+ul {
+  width: 80%;
 }
 </style>
